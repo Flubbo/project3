c16122fbb68cdaacde03619151754e3c27b2902b
Flubbo <lishaidan@gmail.com>
2024-11-09 18:12:51 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ function updatePetCount(){
 
 var doggo = document.getElementById('doggo');
 var petCountDisplay = document.getElementById('petCountDisplay');
-var petCount = 11;
+var petCount = 0;
 doggo.addEventListener('click', increasePetCount);
 doggo.addEventListener('click', updatePetCount);
